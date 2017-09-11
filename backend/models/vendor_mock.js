50f466ec09cdbb6eade0fdc4603bc5c474b4e3fb
Juliafin <juliafinarovsky@gmail.com>
2017-09-10 23:17:25 -0400

    changed some schema stuff and added slice mock

--- backend/models/vendor_mock.js
+++ backend/models/vendor_mock.js
@@ -1,12 +1,13 @@
 
-module.exports = {
-
+module.exports = { 
+  [
+  {
   vendorName: '1871 Grille',
-  foodCategory: 'American',
+  foodCategory: 'Italian',
   menu: [
     {
-      name: 'Chicken Tender Basket',
-      price: 10.00,
+      name: 'Cheese Pizza Slice',
+      price: 5.25,
       vendor: {
         vendorName: '1871 Grille'
       }
@@ -78,5 +79,120 @@ module.exports = {
     }
   
   ] 
-};
+},
 
+
+
+
+{
+vendorName: 'The Slice',
+foodCategory: 'Italian',
+menu: [
+  {
+    name: 'Cheese Pizza Slice',
+    price: 5.25,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: 'Pepperoni Pizza Slice',
+    price: 5.25,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+    
+  },
+
+  {
+    name: 'Breadsticks',
+    price: 7.00,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+  
+  {
+    name: 'Salad with Ranch Dressing',
+    price: 7.00,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: 'Salad with Italian Dressing',
+    price: 7.00,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: 'Salad with French Dressing',
+    price: 7.00,
+    vendor: {
+      vendorName: 'The Slice'
+    } 
+  },
+
+  {
+    name: 'Jumbo All-Beef Hot Dog',
+    price: 6.50,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: `Cannoli`,
+    price: 6.50,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: `Italian Garlic Parmesan Popcorn`,
+    price: 6.50,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: `Peanuts`,
+    price: 5.00,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: `Sour Patch Kids Candy`,
+    price: 5.25,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: `Swedish Fish Candy`,
+    price: 5.25,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  },
+
+  {
+    name: `M&M's Candy`,
+    price: 5.25,
+    vendor: {
+      vendorName: 'The Slice'
+    }
+  }
+]
+
+}
+  ]
