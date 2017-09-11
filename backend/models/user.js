17d4636bc49df968d28db996d559e95121fa5a8c
Juliafin <juliafinarovsky@gmail.com>
2017-09-11 19:09:01 -0400

    wrote location finder function

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,6 +5,9 @@ const productSchema = require('./products');
 
 const userValidation = [/^\w{4,}$/, 'Username must contain only alphanumberic characters'];
 const roleValidation = [/(?=^vendor$)|(?=^customer$)/, 'Role must be a teacher or a student'];
+const vendorValidation = [/(?=^The Slice$)|(?=^1871 Grille$)/, 'Vendor must be The Slice or 1871 Grille']
+
+mongoose.Promise = global.Promise;
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -42,10 +45,15 @@ const userSchema = new mongoose.Schema({
   sectionNumber: {
     type: Number
   },
-  vendorName: {
+  sectionError: {
+    type: String
+  }
 
-  },
   currentOrder: {
+    vendorName: {
+      type: String,
+      match: vendorValidation
+    },
     order: {
       type: [productSchema]
     },
@@ -64,14 +72,14 @@ const userSchema = new mongoose.Schema({
       type: String
     },
     locationNumber: {
-      type:Number
+      type: Number
     }
   },
 
 
 });
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', function (next) {
 
   if (this.role === 'vendor' && !this.password) {
     return next(new Error('The password is required when the role is vendor'));
@@ -80,14 +88,124 @@ userSchema.pre('save', function(next) {
 
 
 
+// assigns the user order to the right location
+userSchema.methods.locationFinder = function () {
+  let userSection = this.sectionNumber;
+  let locationNumber = this.currentOrder.locationNumber;
+  let vendorName = this.currentOrder.vendorName;
 
-userSchema.methods.locationFinder = function
-() {
-  switch(this.sectionNumber && this.vendorName == '1871 Grille') 
-    case: 337-343
-      this.order.locationNumber = 343
+
+  const theSlicerangeArray = [
+    [327, 347, 343],
+    [426, 444, 343],
+    [1, 4, 107],
+    [100, 125, 107],
+    [152, 160, 107],
+    [5, 9, 150],
+    [26, 42, 150],
+    [126, 151, 150],
+    [210, 225, 215],
+    [226, 246, 239],
+    [301, 304, 313],
+    [312, 326, 313],
+    [410, 425, 313],
+    [327, 347, 343],
+  ];
+
+  const grilleRangeArray = [
+    [337, 347, 343],
+    [226, 246, 239],
+    [5, 9, 141],
+    [26, 42, 141],
+    [126, 151, 141],
+    [1, 4, 113],
+    [10, 25, 113],
+    [100, 125, 113],
+    [152, 160, 113],
+    [210, 225, 311]
+  ];
+
+  const grilleSingleArray = [
+    [250, 141],
+    [259, 113],
+    [311, 215]
+  ];
+
+  const theSliceSingleArray = [
+    [259, 107],
+    [250, 150],
+    [311, 215]
+  ];
+
+  // Location Assignment strategy:
+  // 1. If the location number is assigned, return and exit the function
+  // 2. Check if the user selected 1871 Grille and run a loop through the ranges and assign the location number if it matches the section the user selected (userSection)
+  // 3. Return from the function if the location has already been assigned
+  // 4. Check if location has been assigned from the ranges and if it has exit the function
+  // 5. Loop through the single elements to see if one of those matches the location
+  if (locationNumber) {
+    return;
+  } else if (vendorName === "1871 Grille") {
+
+    for (let i = 0; i < grilleRangeArray.length; i++) {
+      if (locationNumber) {
+        return;
+      } else {
+        if (userSection >= grilleRangeArray[i][0] && userSection <= grilleRangeArray[i][1])) {
+        locationNumber = grilleRangeArray[i][2];
+      }
+    }
+  }
+
+  if (locationNumber) {
+    return;
+  } else {
+    for (let j = 0; j < grilleSingleArray.length; j++) {
+      if (locationNumber) {
+        return;
+      } else {
+        if (userSection === grilleSingleArray[j][0]) {
+          locationNumber = grilleSingleArray[j][1]
+        }
+      }
+    }
+
+  }
+
+
+} else if (vendorName === "The Slice") {
+
+  for (let k = 0; k < theSlicerangeArray.length; k++) {
+    if (locationNumber) {
+      return;
+    } else {
+      if (userSection >= theSlicerangeArray[k][0] && userSection <= theSlicerangeArray[k][1])) {
+      locationNumber = theSlicerangeArray[k][2];
+    }
+  }
+}
+
+if (locationNumber) {
+  return;
+} else {
+  for (let m = 0; m < theSliceSingleArray.length; m++) {
+    if (locationNumber) {
+      return;
+    } else {
+      if (userSection === theSliceSingleArray[m][0]) {
+        locationNumber = theSliceSingleArray[m][1]
+      }
+    }
+  }
 
 }
+
+
+} else {
+  this.sectionError = "The supplied section number was invalid."
+}
+
+};
 
 userSchema.methods.showUser = function () {
   return {
@@ -95,7 +213,7 @@ userSchema.methods.showUser = function () {
     first_name: this.first_name,
     last_name: this.last_name,
     id: this._id
-  };  
+  };
 };
 
 userSchema.statics.hashPassword = function (password) {
@@ -108,9 +226,4 @@ userSchema.methods.validatePassword = function (password) {
 
 const User = mongoose.model('user', UserSchema);
 
-module.exports = User;
-
-
-User.locationFinder()
-
-var locationNumber = User.currentOrder.locationNumber
+module.exports = User
