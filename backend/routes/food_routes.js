e00e1e8dea185a90a683f8c0602a52efd4344b24
Server <server@server.com>
2017-09-10 23:00:41 -0400

    restaurant route updated.

--- backend/routes/food_routes.js
+++ backend/routes/food_routes.js
@@ -7,33 +7,36 @@ const Vendor = require("../models/vendor");
 
 mongoose.Promise = global.Promise;
 
-foodRouter.get("/foodlist", function(req, res, listError){
-   if(listError){
-       res.json({status: 404, message: "Bad request"});
+// User gets restaurant list
+foodRouter.get("/foodlist", function(req, res, connectionError){
+   if(connectionError){
+       res.json({status: 502, message: "connection failed"});
    }
    return Vendor.find({})
        .then(function(restaurants){
             res.status(200).json(restaurants.userView());
        })
-       .catch(error){
-        console.log(error); //do something
+       .catch(function (foodError){
+        console.log(foodError); //do something
         return res.status(500).json({"message": "Internal error"});
    }
 
 });
 
-foodRouter.get("/location/:number", function(req, res, locationError){
-   if(locationError){
-       res.status(502).json({"message": "Invalid location."});
+// Vendor gets orders for it's location
+/*foodRouter.get("/location/:number", function(req, res, connectionError){
+   if(connectionError){
+       res.status(502).json({"message": "connection failed."});
    }
    let locationNumber = req.param.number;
-   return Vendor.find({"id": locationNumber})
+   return Vendor.find({"locationNumber": locationNumber})
        .then(function(specificRestaurant) {
-           res.status(200).json(specificRestaurant.userView());
+           forEach(specificRestaurant.orders)
+           res.status(200).json(specificRestaurant);
        })
-       .catch(locationError){
-        console.log(locationError);
+       .catch(function (catchLocationerror){
+        console.log(catchLocationError);
         return res.status(500).json({"message": "Internal error"});}
 
 
-});
+});*/
