ad3289b202f2c10f9b951c841c97302695285700
Juliafin <juliafinarovsky@gmail.com>
2017-09-13 15:47:59 -0400

    wrote mock function

--- backend/models/seedOrder/createOrderobj.js
+++ backend/models/seedOrder/createOrderobj.js
@@ -1,19 +1,22 @@
-module.exports = () => {
+const faker = require('faker');
 
-  const arraySize = Math.floor(Math.random() * 6 );
+let productArray = () => {
+
+  const arraySize = (Math.floor(Math.random() * 6 )) + 2;
   let productArray = [];
   const names = [`M&M's candy`, `Popcorn`, `Hotdog`, `Hamburger`, `Cheese Pizza Slice`, `Curly Fries`, `Burgerizza`, `Peanuts`, `Breadsicks`];
   const vendors = ['The Slice', '1871 Grille'];
   const price = [1.00, 1.25, 1.50, 2.00, 2.25, 2.50, 3.25, 3.50, 4.00, 4.50, 5.00, 5.50, 6.00, 6.50, 7.00, 7.50, 8.00, 9.00, 9.50];
 
+
   for (let i=0; i < arraySize; i++) {
     let product = {
       name: names[Math.floor(Math.random() * names.length )],
-      price: price[Math.floor(Math.random() * price.length )],
+      price: price[Math.floor(Math.random() * price.length )].toFixed(2),
       vendor: {
         vendorName: vendors[Math.floor(Math.random() * vendors.length )]
       },
-      quantityOrdered: Math.floor(Math.random() * 5);
+      quantityOrdered: Math.floor(Math.random() * 5)
     };
 
     productArray.push(product);
@@ -23,3 +26,31 @@ module.exports = () => {
   return productArray;
 
 };
+
+
+
+let order = () => {
+  const role = ['vendor, customer'];
+  let orderObj = {
+  
+    order: productArray(),
+    completed: false,
+    comments: faker.lorem.paragraph(),
+   
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    userName: faker.internet.userName(),
+    password: faker.internet.password(),
+    role: role[Math.floor(Math.random() * role.length)],
+    sectionNumber: Math.floor(Math.random() * 444)
+
+  };
+  console.log(orderObj);
+  return orderObj;
+
+
+  };
+
+
+
+order();
--- backend/models/user.js
+++ backend/models/user.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 4,
-        // match: userValidation,
         trim: true
     },
     password: {
